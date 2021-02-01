235517947d28601cd864d9b7d7cf2fda41b1e960
Orellabac <orellabac@gmail.com>
2021-02-01 16:00:46 -0600

    Create install_informix.sh

---
+++ install_informix.sh
@@ -0,0 +1,5 @@
+mkdir informix
+cd informix
+wget https://iwm.dhe.ibm.com/sdfdl/2v2/regs2/mstadm/informix/Xa.2/Xb.b8S61sgM5_z43bjfRPvdlbQ6xSIoM_LgANQDdCo_QUE/Xc.ibm.ids.14.10.FC5.LNX.tar/Xd./Xf.lPr.D1vk/Xg.11094991/Xi.ifxids/XY.regsrvs/XZ.Ybivain88LOX5JVn__yTtt8p6UOMdWeO/ibm.ids.14.10.FC5.LNX.tar
+tar xvf ibm.ids.14.10.FC5.LNX.tar
+./ids_install -i silent -DLICENSE_ACCEPTED=TRUE
